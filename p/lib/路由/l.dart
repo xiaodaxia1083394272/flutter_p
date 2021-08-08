9436218676bba4523bf8eb5d968332c8ef5ec13a
xiaoyang <1083394272@qq.com>
2021-08-08 23:05:18 +0800

    new

---
+++ p/lib/路由/l.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+//新路由
+class NewRoute extends StatelessWidget {
+
+  void _incrementCounter(){
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New route路径"),
+      ),
+      body: Center(
+        child: Text("This is new route"),
+      ),
+       floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+       )
+    );
+  }
+}
